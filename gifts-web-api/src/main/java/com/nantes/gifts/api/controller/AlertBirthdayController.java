c22221eade3df135344018cc7a1950f5d306f1e5
Romain Mercier <romain.mercier321@gmail.com>
2020-10-21 01:06:58 +0200

    complete AlertBirthday CRUD

--- gifts-web-api/src/main/java/com/nantes/gifts/api/controller/AlertBirthdayController.java
+++ gifts-web-api/src/main/java/com/nantes/gifts/api/controller/AlertBirthdayController.java
@@ -1,14 +1,20 @@
 package com.nantes.gifts.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nantes.gifts.api.bean.AlertBirthday;
+import com.nantes.gifts.api.exception.RessourceNotFoundException;
 import com.nantes.gifts.api.repository.AlertBirthdayRepository;
 
 @RestController
@@ -20,14 +26,42 @@ public class AlertBirthdayController {
 	AlertBirthdayRepository alertBirthdayRepository;
 	
 	
-	//Get all persons
+	//Get all alertBirthday
 	@GetMapping("alertBirthday")
 	public List<AlertBirthday> getAllAlertBirthday(){
 		return this.alertBirthdayRepository.findAll();
 	}
 	
+	//Get alert birthday by id
+	@GetMapping("alertBirthday/{id}")
+	public  ResponseEntity<AlertBirthday> getAlertBirthdayById(@PathVariable(value = "id") Long alerBirthdayId){
+		
+		Optional<AlertBirthday> optAlertBirthday = alertBirthdayRepository.findById(alerBirthdayId);
+		if(optAlertBirthday.isPresent()) {
+			AlertBirthday alertBirthday = optAlertBirthday.get();
+			return ResponseEntity.ok().body(alertBirthday);
+		} else {
+			throw new RessourceNotFoundException("Alert Birthday not found for this id :: " + alerBirthdayId); //return a 404
+		}
+		
+	}
 	
 	
-	
+	//Update AlertBirthday
+	@PutMapping("alertBirthday/{id}")
+	public ResponseEntity<AlertBirthday> updateAlertBirthday(@PathVariable(value = "id") Long alertBirthdayId,
+			@Validated @RequestBody AlertBirthday alertBirthdayDetails) throws RessourceNotFoundException {
+		
+		AlertBirthday alertBirthday = alertBirthdayRepository.findById(alertBirthdayId)
+				.orElseThrow(() -> new RessourceNotFoundException("Alert Birthday not found for this id:: " + alertBirthdayId));
+		
+		alertBirthday.setActive(alertBirthdayDetails.isActive());
+		alertBirthday.setHourAlert(alertBirthdayDetails.getHourAlert());
+		alertBirthday.setFrequency(alertBirthdayDetails.getFrequency());
+		alertBirthday.setDelay(alertBirthdayDetails.getDelay());
+		
+		return ResponseEntity.ok(this.alertBirthdayRepository.save(alertBirthday));
+		
+	}
 
 }
